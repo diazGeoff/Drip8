27b811ae7ce909ca0451833492d2dfee01e5042c
julesmercado <julesarielmercado@gmail.com>
2015-09-02 14:08:03 +0800

    "Changed: fb api id"

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,7 @@ var drip8 = angular.module( "Drip8" , [ ] );
 
 window.fbAsyncInit = function ( ) {
 	FB.init( {
-		"appId"    	 : "719176184895882",
+		"appId"    	 : "1490365664608851",
 	    "status"     : true,
 	    "xfbml"      : true,
 	    "version"    : 'v2.3' // or v2.0, v2.1, v2.0
