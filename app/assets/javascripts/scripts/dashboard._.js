6ec44a6d36af7476a7e31f098965d15e4df7ab4b
julesmercado <julesarielmercado@gmail.com>
2015-10-09 07:54:00 +0800

    "video-dashboard"

--- app/assets/javascripts/scripts/dashboard._.js
+++ app/assets/javascripts/scripts/dashboard._.js
@@ -1,8 +1,9 @@
 drip8
 	.directive( "dripDashboard" , [
 		"$http",
+		"Video",
 		"$rootScope",
-		function directive ( $http , $rootScope ) {
+		function directive ( $http , $rootScope , Video ) {
 			return {
 				"restrict": "A",
 				"scope": true,
@@ -10,11 +11,28 @@ drip8
 					return {
 
 						pre: function pre( scope , element , atrributeSet ){
+							scope.drips = [];
 							scope.queryAllDrips = function queryAllDrips(){
 								$http.get( "/api/drip_length" )
 								.success( function ( response ) {
-									console.log( response )
+									if( response.count != 0 ){
+										scope.count = response.count;
+										var count = 1;
+										scope.responsed = [];
+										for( var index = 3 ; index >= 0 ; index-- ){
+											var id = scope.count - count
+											console.log( id );
+											$http.post( "/api/drip_each" , { "drip_id": id } )
+											.success( function ( responsed ) {
+												scope.responsed.push( responsed );
+												console.log( responsed )
+											} );
+											count++;
+										}
+										
+									}
 								} );
+								scope.$broadcast( 'end-fetch' , scope.responsed );
 							};
 							scope.queryAllDrips();
 						} , 
@@ -22,7 +40,25 @@ drip8
 						post: function post( scope , element , atrributeSet ){
 							scope.queryEachDrip = function queryEachDrip(){
 								//codes to query each drip here
-							}
+							};
+							scope.linkUri = function linkUri ( link , service ) {
+								var video_id = link.split( 'v=' )[1];
+								scope.video_id = video_id;
+								var ampersandPosition = video_id.indexOf( '&' );
+								if ( ampersandPosition != -1 ) {
+								  video_id = video_id.substring( 0 , ampersandPosition );
+								}
+								// console.log( link );
+								// console.log( service );
+								// console.log( Video );
+								// console.log( video_id );
+								if ( service == "thumb" ) {
+
+									//return Video.thumbnail( video_id );
+								} else {
+									return Video.videoSource( video_id );
+								}	
+							};
 						}
 					}
 				}
---
+++ app/assets/javascripts/scripts/video-dashboard._.js
@@ -0,0 +1,18 @@
+drip8
+	.directive( "videoDashboard" , [
+		"$http",
+		"Video",
+		"$rootScope",
+		"$sce"
+		function directive ( $http , Video , $rootScope , $sce ) {
+			return {
+				"restrict": "A",
+				"scope": {
+					"videoSrc": "="
+				},
+				"link": function onLink ( scope , element , attributeSet ) {
+					scope.videoSrc = $sce.trustAsResourceUrl( scope.videoSrc );		
+				}
+			}
+		}
+	] );
